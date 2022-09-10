af35e88b26a8605ad35865633f3cfa429bc50174
Taras Tarasiuk <taras_tarasiuk@epam.com>
2022-09-10 21:20:15 +0300

    Completed Lab1

---
+++ src/main/java/com/kpi/springcourse/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.kpi.springcourse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
