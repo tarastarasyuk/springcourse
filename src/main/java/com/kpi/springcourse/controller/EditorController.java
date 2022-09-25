1ccb5ec639dfe955a15e7712ba79cd72c02459cc
Taras Tarasiuk <taras_tarasiuk@epam.com>
2022-09-25 22:33:14 +0300

    added opportunity functionality

--- src/main/java/com/kpi/springcourse/controller/EditorController.java
+++ src/main/java/com/kpi/springcourse/controller/EditorController.java
@@ -1,22 +1,57 @@
 package com.kpi.springcourse.controller;
 
+import com.kpi.springcourse.model.Opportunity;
 import com.kpi.springcourse.service.EditorService;
+import com.kpi.springcourse.service.OpportunityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/editor")
+@RequestMapping("/editor/opportunities")
 public class EditorController {
 
     @Autowired
     private EditorService editorService;
+    @Autowired
+    private OpportunityService opportunitiesService;
 
-    @GetMapping()
-    public String getAll(Model model) {
-        model.addAttribute("editors", editorService.findAll());
-        return "editors";
+    @GetMapping("")
+    public String getAll(Model model, @RequestParam(value = "sort", required = false) String sort) {
+        model.addAttribute("opportunities", opportunitiesService.findAll(sort));
+        return "opportunities/table";
     }
+
+    @GetMapping("/create")
+    public String getCreationFormOpportunity(@ModelAttribute("opportunityToCreate") Opportunity opportunityToCreate) {
+        return "opportunities/create";
+    }
+
+    @PostMapping("/create")
+    public String createOpportunity(@ModelAttribute("opportunityToCreate") Opportunity opportunityToCreate) {
+        long id = opportunitiesService.create(opportunityToCreate).getId();
+        return "redirect:/editor/opportunities#" + id;
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editOpportunity(Model model, @PathVariable Long id) {
+        Opportunity opportunityToUpdate = opportunitiesService.findById(id);
+        model.addAttribute("opportunityToEdit", opportunityToUpdate);
+        return "/opportunities/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editOpportunity(@ModelAttribute("opportunityToEdit") Opportunity opportunityToUpdate, @PathVariable Long id) {
+        Opportunity targetOpportunity = opportunitiesService.findById(id);
+        opportunitiesService.update(opportunityToUpdate, targetOpportunity);
+        return "redirect:/editor/opportunities#" + id;
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteOpportunity(@PathVariable Long id) {
+        opportunitiesService.delete(id);
+        return "redirect:/editor/opportunities";
+    }
+
 }
--- src/main/java/com/kpi/springcourse/controller/OpportunityController.java
+++ src/main/java/com/kpi/springcourse/controller/OpportunityController.java
@@ -34,40 +34,4 @@ public class OpportunityController {
         return "redirect:/opportunities#" + id;
     }
 
-    @GetMapping("/create")
-    public String getCreationFormOpportunity(@ModelAttribute("opportunityToCreate") Opportunity opportunityToCreate) {
-        return "opportunities/create";
-    }
-
-    @PostMapping("/create")
-    public String createOpportunity(@ModelAttribute("opportunityToCreate") Opportunity opportunityToCreate) {
-        long id = opportunitiesService.create(opportunityToCreate).getId();
-        return "redirect:/opportunities#" + id;
-    }
-
-    @GetMapping("/edit")
-    public String table(Model model) {
-        model.addAttribute("opportunities", opportunitiesService.findAll());
-        return "opportunities/table";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String edit(Model model, @PathVariable Long id) {
-        Opportunity opportunityToUpdate = opportunitiesService.findById(id);
-        model.addAttribute("opportunityToEdit", opportunityToUpdate);
-        return "opportunities/edit";
-    }
-
-    @PostMapping("/edit/{id}")
-    public String edit(@ModelAttribute("opportunityToEdit") Opportunity opportunityToUpdate, @PathVariable Long id) {
-        Opportunity targetOpportunity = opportunitiesService.findById(id);
-        opportunitiesService.update(opportunityToUpdate, targetOpportunity);
-        return "redirect:/opportunities#" + id;
-    }
-
-    @PostMapping("/delete/{id}")
-    public String delete(@PathVariable Long id) {
-        opportunitiesService.delete(id);
-        return "redirect:/opportunities";
-    }
 }
