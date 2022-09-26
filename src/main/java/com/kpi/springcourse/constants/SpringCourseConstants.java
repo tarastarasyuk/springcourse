ba470d9f0384b40bb454d0fb86d5b5856276df65
Taras Tarasiuk <taras_tarasiuk@epam.com>
2022-09-26 22:29:14 +0300

    added simple auth filter to restrict users

---
+++ src/main/java/com/kpi/springcourse/constants/SpringCourseConstants.java
@@ -0,0 +1,6 @@
+package com.kpi.springcourse.constants;
+
+public class SpringCourseConstants {
+    public static final String SESSION_AUTH_ATTR = "userEmail";
+    public static final String SESSION_AUTH_ATTR_ROLE = "userRole";
+}
