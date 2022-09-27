c5755231b7d4124fd8ebe6bab899bac8c702c599
itzRoma <romabondarenko03@gmail.com>
2022-09-27 12:47:27 +0300

    added basic form validation & fixed opportunity tab for editors

--- src/main/java/com/kpi/springcourse/model/Opportunity.java
+++ src/main/java/com/kpi/springcourse/model/Opportunity.java
@@ -2,12 +2,14 @@ package com.kpi.springcourse.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Opportunity {
     private Long id;
     private String name;
